1ffbc1cd58a25b2fc40b830af39c6501dafa90c7
Peduxx <phalves944@gmail.com>
2024-02-23 04:29:26 -0300

    shared

---
+++ src/Shared/Http/Response.cs
@@ -0,0 +1,28 @@
+﻿namespace Shared.Http
+{
+    public class Response
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public object Data { get; set; }
+
+        public Response() { }
+
+        public Response SetSuccess(object? data = null, string? message = "")
+        {
+            Success = true;
+            Message = message;
+            Data = data ?? Array.Empty<object>();
+            return this;
+        }
+
+        public Response SetFailure(string message)
+        {
+            Success = false;
+            Message = message;
+            Data = Array.Empty<object>();
+            return this;
+        }
+    }
+
+}
